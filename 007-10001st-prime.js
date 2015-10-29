bbef6936e43738b409863f40014af7ac10756c8e
Greg Van Buren <g.vanburen@gmail.com>
2015-10-29 07:02:24 -0700

    question 7

---
+++ 007-10001st-prime.js
@@ -0,0 +1,22 @@
+/*By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10 001st prime number?*/
+
+function primeNum(n){
+  var arr = [2];
+  for(var i = 3; arr.length < n; i++){
+    if(isPrime(i)){
+      arr.push(i);
+    }
+  }
+  console.log(arr[n-1]);
+}
+
+function isPrime(n){
+  for (var i = 2; i < n; i++){
+    if(n % i === 0){
+      return false;
+    }
+  }
+  return true;
+}
