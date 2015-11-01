18a6422ba929d02ab492d505afa76cc1e37facd5
Greg Van Buren <g.vanburen@gmail.com>
2015-11-01 10:00:54 -0800

    question 10

---
+++ 010-summation-of-primes.js
@@ -0,0 +1,24 @@
+/*The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.*/
+
+function primeSum(n){
+  var sum = 2;
+  for(var i = 3; i < n; i++){
+    if(isPrimeNum(i)){
+      sum+=i;
+    }
+  }
+  console.log(sum);
+}
+
+function isPrimeNum(n){
+  if(n < 2){return false}
+  var prime = true;
+  for(var i = 2; i <= Math.sqrt(n); i++){
+    if(n % i === 0){
+      prime = false;
+    }
+  }
+  return prime;
+}
