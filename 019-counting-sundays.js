25713e7aa83a581a8aa7931f9a892f1c5ed9b1fc
Greg Van Buren <g.vanburen@gmail.com>
2015-11-10 06:48:04 -0800

    question 19

---
+++ 019-counting-sundays.js
@@ -0,0 +1,21 @@
+/*You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?*/
+
+var count = 0;
+for(var year = 1901; year <= 2000; year++){
+  for(var month = 0; month <= 11; month++){
+    if(new Date(year, month).getDay() == 0){
+      count++;
+    }
+  }
+}
+console.log(count);
