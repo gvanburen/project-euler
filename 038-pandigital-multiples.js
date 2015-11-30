2b06bf8e21cde31202cc03e5f3ea439e36d61873
Greg Van Buren <g.vanburen@gmail.com>
2015-11-29 17:59:24 -0800

    question 38

---
+++ 038-pandigital-multiples.js
@@ -0,0 +1,45 @@
+/*Take the number 192 and multiply it by each of 1, 2, and 3:
+
+192 × 1 = 192
+192 × 2 = 384
+192 × 3 = 576
+By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)
+
+The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
+
+What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n > 1?*/
+
+function isPandigital(a) {
+    var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var test = a;
+    test = test.split("").sort();
+    if (test.length > 9) {
+        return false;
+    }
+    var i = numbers.length;
+    if ((i === 0) || (i !== test.length)) {
+        return false;
+    }
+    while (i--) {
+        if (numbers[i] !== test[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var max = 0;
+for (var n = 1; n < 1000000; n++) {
+    var str = "";
+    for (var j = 1; j < 9; j++) {
+        var d = n * j;
+        str += String(d);
+        if (isPandigital(str)) {
+            if (Number(str) > max) {
+                max = Number(str);
+            }
+            break;
+        }
+    }
+}
+console.log(max);
