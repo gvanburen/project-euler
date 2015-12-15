09f1fe1ec74aec1faab67df3ef82f7551eb015dd
Greg Van Buren <g.vanburen@gmail.com>
2015-12-15 07:25:05 -0800

    question 52

---
+++ 052-permuted-multiples.js
@@ -0,0 +1,29 @@
+/*It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+
+Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.*/
+
+function isPermutation(a, b){
+	var a = String(a).split("").sort();
+	var b = String(b).split("").sort();
+    for (var i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+var notFound = true;
+var i = 1;
+while(notFound){
+	if(isPermutation(i, i*2) &&
+		isPermutation(i, i*3) &&
+		isPermutation(i, i*4) &&
+		isPermutation(i, i*5) &&
+		isPermutation(i, i*6)
+		)
+	{
+		console.log(i);
+		notFound = false;
+		break;
+	}
+}
